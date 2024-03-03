5111dbb2dcc28f7a9d2ec487e4bc75bb743e9680
jonscrp <134547933+jonscrp@users.noreply.github.com>
2024-03-02 22:17:01 -0500

    data object function

--- react-native-app/components/SwipeScreen.js
+++ react-native-app/components/SwipeScreen.js
@@ -6,6 +6,10 @@ import { Button } from "react-native";
 import { useEffect, useState } from "react";
 import React from 'react'
 
+
+import jsonData from "C:/Users/jsuco/OneDrive/Desktop/Hackathons/perfectoutfit/react-native-app/components/sampleCardsData.json";
+console.log(jsonData.data[0].categories);
+
 export default function SwipeScreen({ navigation }) {
   return (
     <View style={styles.container}>
@@ -15,38 +19,13 @@ export default function SwipeScreen({ navigation }) {
   );
 }
 
-
-
-
-const sampleCardsData=[
-  {
-    id: 0,
-    name: 'macys black pants 013412',
-    displayText: `H&M 
-    Men's Baggy Jeans Black`,
-    images: {
-        front: 'https://lp2.hm.com/hmgoepprod?set=format%5Bwebp%5D%2Cquality%5B79%5D%2Csource%5B%2Fa9%2Fc9%2Fa9c948b779d1d73c401e3dc5325a9ddf8bc82f0a.jpg%5D%2Corigin%5Bdam%5D%2Ccategory%5B%5D%2Ctype%5BDESCRIPTIVESTILLLIFE%5D%2Cres%5Bm%5D%2Chmver%5B2%5D&call=url%5Bfile%3A%2Fproduct%2Fmain%5D',
-        back: ''
-    },
-    description: {
-      Length: "Long",
-      WaistRise: "Regular waist",
-      Fit: "Loose fit",
-      Style: "Balloon Leg, Stacked",
-      Description: "Dark denim gray, Solid-color",
-      Imported: "Yes",
-      Concept: "DENIM"
-    }
-  }
-]
-
 for (let i=1 ; i <= 10; i++) {
   sampleCardsData.push({...sampleCardsData[0]})
   sampleCardsData[i].id=i
 }
 
 function CardSwiper( {navigation} ) {
-''
+
   const cardsData = [...sampleCardsData];
 
   // const likedItems = [];
---
+++ react-native-app/components/testingData.js
@@ -0,0 +1,62 @@
+const jsonData  = require ("C:/Users/jsuco/OneDrive/Desktop/Hackathons/perfectoutfit/react-native-app/components/sampleCardsData.json");
+
+
+// // genders 2
+console.log(jsonData.data.length);
+// console.log(jsonData.data[0].gender);
+// console.log(jsonData.data[1].gender);
+
+// // 4 categories
+// console.log(jsonData.data[0].categories[0].category);
+// console.log(jsonData.data[0].categories[1].category);
+// console.log(jsonData.data[0].categories[2].category);
+// console.log(jsonData.data[0].categories[3].category);
+
+// console.log(jsonData.data[0].categories[0].images[0])
+
+
+
+console.log(dataItems());
+
+
+function dataItems(){
+    items  = [];
+    //console.log(jsonData);
+    uniqueIndex = 0;
+    
+    for(let genderIndex = 0; genderIndex < jsonData.data.length; genderIndex++){
+        //console.log("IRUN");
+        for(let categoryIndex = 0; categoryIndex < jsonData.data[genderIndex].categories.length; categoryIndex++){
+            for( let imageIndex = 0; imageIndex < jsonData.data[genderIndex].categories[categoryIndex].images.length; imageIndex++){
+                items.push({
+
+                    id: uniqueIndex,
+                    gender: jsonData.data[genderIndex].gender,
+                    category: jsonData.data[genderIndex].categories[categoryIndex].category,
+                    imagePath:  jsonData.data[genderIndex].categories[categoryIndex].images[imageIndex]
+                });
+                uniqueIndex++;
+            }
+        }
+    }
+    
+    return items;
+}
+
+
+
+
+
+
+
+
+
+//function gettingItems(){
+    
+  //   data : jsonData.data;
+
+
+     
+    //}
+
+//}
